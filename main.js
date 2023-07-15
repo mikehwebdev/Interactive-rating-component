9ae2ce1c4e13032ccc302337a6cbf57ae60c2d7a
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-07-15 07:58:01 +0100

    Finished styling, js adjustment, html adjustment

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const buttons = document.querySelectorAll('.score-btn')
 const buttonContainer = document.getElementById('buttons-container')
 const submitButton = document.querySelector('.submit-btn')
-
 const screenOne = document.getElementById('screen-one')
 const screenTwo = document.getElementById('screen-two')
 const feedBackScore = document.getElementById('feedback-score')
 
+let score = 0
+
 buttonContainer.addEventListener('click', (e)=>{
     for (let button of buttons) {
         if (button === e.target) {
-            feedBackScore.textContent =  `You selected ${Number(e.target.textContent)} out of 5`  
+            score = Number(e.target.textContent)
+            feedBackScore.textContent =  `You selected ${score} out of 5`  
             button.classList.add('selected')
         } else {
             button.classList.remove('selected')
@@ -18,7 +20,9 @@ buttonContainer.addEventListener('click', (e)=>{
 })
 
 submitButton.addEventListener('click', ()=>{
+    if (score !== 0) {
     screenOne.style.display = 'none'
     screenTwo.style.display = 'flex'
+    }
 })
 
